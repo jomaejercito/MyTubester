dea6e43d03e6847f4dcdc045dba4ebd4eba84e24
Joma Ejercito <jomaejercito@gmail.com>
2019-10-15 16:42:13 -0400

    Add a user seed for testing

--- mytubester-api/db/seeds.rb
+++ mytubester-api/db/seeds.rb
@@ -2,7 +2,9 @@
 #   {username: 'joma' , email: 'joma@gmail.com' , password_digest: 'password'}
 # ]
 
+User.create(username: 'joma', email: 'joma@gmail.com' , password: 'password')
+
 # videos = [
-#   {title: 'Britney Spears - Toxic (Official Music Video)', video_url: 'https://www.youtube.com/watch?v=LOZuxwVk7TU', description: '', duration: , uploaded_by: '
-#   Britney Spears', uploaded_at: }
+#   {title: 'Britney Spears - Toxic (Official Music Video)', video_url: 'https://www.youtube.com/watch?v=LOZuxwVk7TU', description: 'Britney Spears' official music video for 'Toxic'.', duration: 3:31, uploaded_by: 'Britney Spears
+#   Britney Spears', uploaded_at: 10-25-2009, views: 411,339,913 }
 # ]
