1fab07914c9eac91703148618302ec39138333f2
Joma Ejercito <jomaejercito@gmail.com>
2019-10-15 19:59:37 -0400

    Set up index.js and create store

--- mytubester-client/src/index.js
+++ mytubester-client/src/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let reducer = ''
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
