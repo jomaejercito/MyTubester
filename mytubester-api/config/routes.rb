dbdcab6b1cabb5b14c90afdf2ce15110eb8bfd41
Joma Ejercito <jomaejercito@gmail.com>
2019-10-15 22:53:39 -0400

    Edit routes

--- mytubester-api/config/routes.rb
+++ mytubester-api/config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
-      resources :users do
-        resources :videos
-      end
+      resources :users
+      resources :videos
     end
   end
   
